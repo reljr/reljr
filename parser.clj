9a374ac9a6e1317235b06b41f6cf36b7cc772dd5
Luciano Laratelli <luciano@laratelli.com>
2020-11-11 20:17:00 -0500

    move parser to its own files
    thanks, kiran

--- parser.clj
+++ parser.clj
@@ -1,42 +1,6 @@
 (ns parser.core
   (:require [instaparse.core :as insta]))
 
-;; grammar conventions:
-;;
-;;   - CamelCase is used for NonTerminals
-;;   - snake_case is used for terminals
-;;
-;;   when a NonTerminal introduces a new terminal, it is immediately defined
-;;   after that rule.
-;;
-;;   NonTerminals are defined in the order they are introduced, unless they are
-;;   common to many NonTerminals. In that case, they are defined at the bottom
-;;   of the grammar.
-;;
-
-
 (def relational-algebra-parser
-  (insta/parser
-   "S                = RAExpression
+  (insta/parser (clojure.java.io/resource "RAParser.bnf")))
 
-    RAExpresssion    = '(' RAExpression ')'                               |
-                       '(' RAExpression ')' TopLevelOperator RAexpression |
-                       RelationName                                       |
-                       RelationName MidLevelOperator RAExpression         |
-                       TopLevelOperator RAExpression                      |
-                       TopLevelOperator RAExpression MidLevelOperator RAExpression
-
-    TopLevelOperator = Projection     |
-                       Selection      |
-                       RenameRelation |
-                       RenameColumn   |
-                       OrderBy        |
-                       GroupBy        |
-
-    MidLevelOperator = Intersection |
-                       Union        |
-                       Division     |
-                       Subtraction  |
-                       CrossProduct |
-                       NaturalJoin
- "))
