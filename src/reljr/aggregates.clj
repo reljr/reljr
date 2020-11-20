e7b74a4d319eb7ef98abdec42f39293480774560
Luciano Laratelli <luciano@laratelli.com>
2020-11-20 18:56:25 -0500

    implement aggregates; add their tests

--- src/reljr/aggregates.clj
+++ src/reljr/aggregates.clj
@@ -1,29 +1,18 @@
 (ns reljr.aggregates)
 
-(def some-table
-  #{{:R/a 1 :R/b 2 :R/c 3}
-    {:R/a 1 :R/b 7 :R/c 2}
-    {:R/a 2 :R/b 5 :R/c 1}})
-
-(defn min [table column]
+(defn mincol [table column]
   (reduce clojure.core/min (map column table)))
 
-(defn max [table column]
+(defn maxcol [table column]
   (reduce clojure.core/max (map column table)))
 
 (defn sum [table column]
   (reduce + (map column table)))
 
-(defn cnt [table column]
+(defn cntcol [table column]
   (reduce clojure.core/count table))
 
 (defn avg [table column]
 
   (/ (sum table column)
      (clojure.core/count table)))
-
-(sum some-table :R/c)
-
-(avg some-table :R/b)
-
-(cnt some-table :R/b)
