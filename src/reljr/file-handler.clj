c844acde58c1fbccc432197729d200aa49094a7a
Luciano Laratelli <luciano@laratelli.com>
2020-11-22 00:49:27 -0500

    start sketching out get-table

--- src/reljr/file-handler.clj
+++ src/reljr/file-handler.clj
@@ -3,11 +3,23 @@
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as str]))
 
-(defn get-table-name [filename]
+(defn get-table-name
+  [filename]
   (first (str/split filename #"\.")))
 
-(defn get-file [filename]
-  {(get-table-name filename)})
 
-(with-open [reader (io/resource "foo.csv")]
-  (doall (csv/read-csv reader)))
+;;need to map each member of table-headers to be namespaced by table-name, and
+;;then map each of those to the values in row
+
+
+(defn get-table [filename table]
+  (let [table-name (get-table-name filename)
+        table-headers (first table)
+        table-rows (rest table)
+        ids (map keyword table-name table-headers)]
+    (for [row table-rows]
+      ids)))
+
+(get-table "foo.csv"
+           (with-open [reader (io/reader (io/resource "foo.csv"))]
+             (doall (csv/read-csv reader))))
