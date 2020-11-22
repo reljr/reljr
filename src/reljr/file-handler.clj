0638d1730a6b54428916fd1a243581302dfde49f
Luciano Laratelli <luciano@laratelli.com>
2020-11-22 02:05:15 -0500

    just need to namespace the columns and we're good

--- src/reljr/file-handler.clj
+++ src/reljr/file-handler.clj
@@ -1,4 +1,4 @@
-(ns reljr.file
+(ns reljr.file-handler
   (:require [clojure.data.csv :as csv])
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as str]))
@@ -7,19 +7,16 @@
   [filename]
   (first (str/split filename #"\.")))
 
+(defn csv-data->maps [csv-data]
+  ;;boilerplate from https://github.com/clojure/data.csv
+  (map zipmap
+       (->> (first csv-data) ;; First row is the header
+            (map keyword) ;; Drop if you want string keys instead
+            repeat)
+       (rest csv-data)))
 
-;;need to map each member of table-headers to be namespaced by table-name, and
-;;then map each of those to the values in row
-
-
-(defn get-table [filename table]
-  (let [table-name (get-table-name filename)
-        table-headers (first table)
-        table-rows (rest table)
-        ids (map keyword table-name table-headers)]
-    (for [row table-rows]
-      ids)))
-
-(get-table "foo.csv"
-           (with-open [reader (io/reader (io/resource "foo.csv"))]
-             (doall (csv/read-csv reader))))
+(defn get-table-data
+  [filename]
+  {(get-table-name filename)
+   (csv-data->maps (with-open [reader (io/reader (io/resource filename))]
+                     (doall (csv/read-csv reader))))})
