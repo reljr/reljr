ae4002e3fa9387a3ef02382b6368c9095adf51e3
Elijah Malaby <qwe12345678910@gmail.com>
2020-11-27 19:26:13 -0500

    Add commands to grammar

--- src/reljr/parser.clj
+++ src/reljr/parser.clj
@@ -1,5 +1,8 @@
 (ns reljr.parser
   (:require [instaparse.core :as insta]))
 
-(def relational-algebra-parser
+(def full-relational-algebra-parser
   (insta/parser (clojure.java.io/resource "RAParser.bnf")))
+
+(defn relational-algebra-parser [text]
+  (full-relational-algebra-parser text :start :RAExpression))
